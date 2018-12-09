fff511337bfbe5ea382270927d146ab797ef9862
David Bell <dbelldev@icloud.com>
2018-12-09 14:08:26 +0000

    map added

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,13 +7,16 @@ class Map extends Component {
 
   render(){
     return(
-      <>
-      <div className="click-map">
-      <h1>Dinosaur Island</h1>
-      <p>Click on a paddock to check inhabitants</p>
-      <p>INSERT IMAGE HERE</p>
+      <div className="map-container">
+        <img src="../../public/images/isla_nublar.jpg" usemap="#image-map"/>
+        <map name="image-map">
+          <area target="" alt="" title="" href="" coords="82,111,312,269" shape="rect"/>
+          <area target="" alt="" title="" href="" coords="333,98,569,288" shape="rect"/>
+          <area target="" alt="" title="" href="" coords="185,299,316,468" shape="rect"/>
+          <area target="" alt="" title="" href="" coords="341,320,542,477" shape="rect"/>
+          <area target="" alt="" title="" href="" coords="337,559,68" shape="circle"/>
+        </map>
       </div>
-      </>
     )
 
   }
