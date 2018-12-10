504e24d59baed0efb0bf53cfa3f9c1e329bead4d
Anna Henderson <annahenderson@gmail.com>
2018-12-10 09:58:38 +0000

    found dino images

--- src/components/Map.js
+++ src/components/Map.js
@@ -17,11 +17,11 @@ handleChange(evt){
       <div className="map-container">
         <img src="./images/isla_nublar.jpg" useMap="#image-map"/>
         <map name="image-map">
-          <area target="" alt="Bay View" title= "3" href="" coords="82,111,312,269" shape="rect" onClick = {this.handleChange}/>
-          <area target="" alt="" title="" href="" coords="333,98,569,288" shape="rect"/>
-          <area target="" alt="" title="" href="" coords="185,299,316,468" shape="rect"/>
-          <area target="" alt="" title="" href="" coords="341,320,542,477" shape="rect"/>
-          <area target="" alt="" title="" href="" coords="337,559,68" shape="circle"/>
+          <area alt="Bay View" title= "3" coords="82,111,312,269" shape="rect" onClick = {this.handleChange}/>
+          <area alt="Green Hills" title="2" coords="333,98,569,288" shape="rect" onClick = {this.handleChange}/>
+          <area alt="Sunny Acres" title="1" coords="185,299,316,468" shape="rect" onClick = {this.handleChange}/>
+          <area alt="Lake Side" title="4" coords="341,320,542,477" shape="rect" onClick = {this.handleChange}/>
+          <area alt="Fishers Point" title="5" coords="337,559,68" shape="circle" onClick = {this.handleChange}/>
         </map>
       </div>
     )
--- src/components/PaddockGuests.js
+++ src/components/PaddockGuests.js
@@ -9,16 +9,13 @@ const paddockGuests = props.paddockInfo._embedded.dinosaurs.map((dinosaur, index
     return(
 
     <div className="dinosaur-short" key={index}>
-      <p>{dinosaur.name}</p>
-      <p>{dinosaur.dinoSpecies}</p>
-      <p>{dinosaur.dinoDietType}</p>
+      <p>Name: {dinosaur.name}</p>
+      <p>Species: {dinosaur.dinoSpecies}</p>
+      <p>Diet: {dinosaur.dinoDietType}</p>
     </div>
   )
 })
-
       return(
-
-
         <>
         <div className="paddock-guests-title">
         <h1>Paddocks Inhabitants</h1>
