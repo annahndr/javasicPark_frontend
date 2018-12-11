ee8db3163c45202971706957a5275759181c5426
David Bell <dbelldev@icloud.com>
2018-12-11 12:50:52 +0000

    activate bugs fixed

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -61,11 +61,7 @@ render(){
     console.log("render started");
   return (
     <>
-<<<<<<< HEAD
-      <Navbar paddock = {this.state.paddock} dinosaurs = {this.state.dinosaurs} paddocks = {this.state.paddocks} getDinosaurs = {this.getDinosaurs} getPaddocks={this.getPaddocks} getPaddocksResetToUnfed={this.getPaddocksResetToUnfed}/>
-=======
-      <Navbar getPaddock = {this.getPaddock} paddock = {this.state.paddock} dinosaurs = {this.state.dinosaurs} paddocks = {this.state.paddocks} getDinosaurs = {this.getDinosaurs}/>
->>>>>>> feature/adddinosaur
+      <Navbar getPaddock = {this.getPaddock} paddock = {this.state.paddock} dinosaurs = {this.state.dinosaurs} paddocks = {this.state.paddocks} getDinosaurs = {this.getDinosaurs} getPaddocks={this.getPaddocks}/>
     </>
     )
   }
